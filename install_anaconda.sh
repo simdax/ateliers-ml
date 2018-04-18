2a7a8eaed0707dd2f7bb2fc69c54139350eb75cf
Simon CORNAZ <scornaz@e1r11p20.42.fr>
2018-04-18 12:02:51 +0200

    fix path for anaconda.sh

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -7,7 +7,7 @@ mkdir $Perso
 chmod 700 $Perso
 cd $Perso
 curl -o anaconda3.sh https://repo.continuum.io/archive/Anaconda3-5.0.1-MacOSX-x86_64.sh
-sh anaconda3.sh
+sh anaconda3.sh -p /sgoinfre/goinfre/Perso/$Perso/anaconda3/bin
 echo
 echo export PATH="/sgoinfre/goinfre/Perso/$Perso/anaconda3/bin:$PATH" >> ~/.zshrc
 source ~/.zshrc
