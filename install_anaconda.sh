0b1653ce6a6d77744dae1b81080288d7bbfa0077
Simon CORNAZ <scornaz@e1r11p20.42.fr>
2018-04-18 11:50:16 +0200

    correct one coquille and add install script

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -1,11 +1,14 @@
+#!/bin/sh
+
 Perso=`whoami`
 
 cd /sgoinfre/goinfre/Perso
-mkdir VOTRE_LOGIN
-chmod 700 VOTRE_LOGIN
+mkdir $Perso
+chmod 700 $Perso
 cd $Perso
 curl -o anaconda3.sh https://repo.continuum.io/archive/Anaconda3-5.0.1-MacOSX-x86_64.sh
 sh anaconda3.sh
+echo
 echo export PATH="/sgoinfre/goinfre/Perso/$Perso/anaconda3/bin:$PATH" >> ~/.zshrc
 source ~/.zshrc
 echo devrait afficher
